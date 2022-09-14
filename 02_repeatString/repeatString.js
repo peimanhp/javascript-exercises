d3375f41b6cc12a813e39a94b3a2e532d8d28732
Peiman <peiman_hp@yahoo.com>
2022-09-15 00:51:17 +0430

    repeatString done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
-
-};
+function repeatString(string, num) {
+  let words;
+  if (string == "") {
+    words = "";
+  } else if (num < 0) {
+    words = "ERROR";
+  } else if (num == 0) {
+    words = "";
+  } else if (num > 0) {
+    words = string;
+    for (let i = 0; i < num - 1; i++) {
+      words = words + string;
+    }
+  }
+  return words;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
