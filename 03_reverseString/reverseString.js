a9a049b14f17821c031dd24b3ecd99929457f18a
Peiman <peiman_hp@yahoo.com>
2022-09-17 00:53:50 +0430

    reverseString done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
-};
+function reverseString(_words) {
+  let words = _words;
+  const wordslength = words.length;
+  const lettersArray = words.split("");
+  let reverseWords = "";
+  for (let i = wordslength - 1; i >= 0; i--) {
+    reverseWords += lettersArray[i];
+  }
+  return reverseWords;
+} // all tests passed with single try!!!!!
 
 // Do not edit below this line
 module.exports = reverseString;
