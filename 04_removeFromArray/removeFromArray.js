eef91df23856a99816c9ad565c4a7868d77dd770
Peiman <peiman_hp@yahoo.com>
2022-09-17 14:10:13 +0430

    removeFromArray done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
-
-};
+function removeFromArray() {
+  const arguArr = [...arguments]; // create an array from arguments of function
+  let arguTypes = Array.from([...arguments], (x) => typeof x); // create an array from type of aguments
+  let isArrayArgs = Array.from([...arguments], (x) => Array.isArray(x)); // create a boolean array from witch elemts is array (not object!)
+  let arrayInArgs;
+  for (let i = 0; i < arguTypes.length; i++) {    // splice array element from arguments
+    if (isArrayArgs[i]) {
+      arrayInArgs = arguArr.splice(i, 1);
+    }
+  }
+  let newArr = arrayInArgs.shift(); // bring out array element from agument array
+  newArr = newArr.filter((val) => !arguArr.includes(val)); // compare two array and remove same elements from first array
+  return newArr;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
