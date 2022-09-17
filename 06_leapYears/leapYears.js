96e996b9427d4ff8e5003cd01b658a0ef7351bfa
Peiman <peiman_hp@yahoo.com>
2022-09-17 17:49:18 +0430

    leapYear done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,11 @@
-const leapYears = function() {
-
-};
+function leapYears(_year) {
+  let year = _year;
+  if (year % 4 == 0) {
+    if (year % 100 != 0 || year % 400 == 0) {
+      return true;
+    } else return false;
+  } else return false;
+}
 
 // Do not edit below this line
 module.exports = leapYears;
