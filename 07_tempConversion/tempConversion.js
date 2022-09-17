3042ed21975948fb85fb6b18fba4189a22895f70
Peiman <peiman_hp@yahoo.com>
2022-09-17 22:50:17 +0430

    tempConversation done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,17 @@
-const ftoc = function() {
+function ftoc(_degree) {
+  let degree = _degree;
+  let converted = (degree - 32) * 5 / 9;
+  return Math.round(converted * 10) / 10;
+}
 
-};
-
-const ctof = function() {
-
-};
+function ctof(_degree) {
+  let degree = _degree;
+  let converted = (degree * 9 / 5) + 32;
+  return Math.round(converted * 10) / 10;
+}
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
