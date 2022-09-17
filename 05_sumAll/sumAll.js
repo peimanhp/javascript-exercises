fe829d31a8ce15b00bc5a90cb6a84b29cebbe058
Peiman <peiman_hp@yahoo.com>
2022-09-17 15:36:17 +0430

    sumAll done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
-};
+function sumAll(a, b) {
+  if (a < 0 || b < 0 || typeof a != "number" || typeof b != "number") {
+    return "ERROR";
+  } else {
+    let min;
+    let max;
+    if (a < b) {
+      min = a;
+      max = b;
+    } else {
+      min = b;
+      max = a;
+    }
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+      sum += i;
+    }
+    return sum;
+  }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
