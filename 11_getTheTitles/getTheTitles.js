8b91e515afbc4e4479d0c2815792a9a827ac3e6c
Peiman <peiman_hp@yahoo.com>
2022-09-19 01:07:18 +0430

    getTheTitles done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (booksArray) {
+  let titlesArray = [];
+  for (let i = 0; i < booksArray.length; i++) {
+    titlesArray.push(booksArray[i].title);
+    }
+    return titlesArray;
 };
 
 // Do not edit below this line
