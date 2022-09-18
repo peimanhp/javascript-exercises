1780c961361f54538b17b5483c831d776e364925
Peiman <peiman_hp@yahoo.com>
2022-09-18 17:48:09 +0430

    palindromes done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (_words) {
+  let words = _words.replace(/[.,\/#!?$%\^&\*;:{}=\-_`~()]/g, ""); // removes punctuation from string
+  let arrayWords = words.split(" "); // splites words and return array of it
+  let lastWord = arrayWords[arrayWords.length - 1].toLowerCase();
+  let noSpacesWords = words.replace(/\s+/g, "").toLowerCase(); // removes space from original string
+  let reverseWord = "";
+  for (let i = lastWord.length - 1; i >= 0; i--) {
+    reverseWord += lastWord.charAt(i);
+  }
+  return noSpacesWords.startsWith(reverseWord);
 };
 
 // Do not edit below this line
