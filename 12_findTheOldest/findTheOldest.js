7782dddaa53aa26d4efad11918b43a38b63cc265
Peiman <peiman_hp@yahoo.com>
2022-09-22 11:37:20 +0330

    caesar done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const getAge = (yearOfBirth, yearOfDeath) => {
+    if (!yearOfDeath) {
+      yearOfDeath = new Date().getFullYear();
+      return yearOfDeath - yearOfBirth;
+    } else return yearOfDeath - yearOfBirth;
+  };
 
+  let oldestPerson = people.reduce((previousPerson, currentPerson) => {
+    previousAge = getAge(
+      previousPerson.yearOfBirth,
+      previousPerson.yearOfDeath
+    );
+    currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+    if (currentAge > previousAge) return currentPerson;
+    else return previousPerson;
+  });
+  return oldestPerson.name;
 };
 
 // Do not edit below this line
--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,24 @@
-const caesar = function() {
-
+const caesar = function (str, num) {
+  this.str = str;
+  let unicodeArray = str.split("").map((letter) => letter.charCodeAt(0));
+  let newUnicodeArray = [];
+  for (const letter of unicodeArray) {
+    if (num > 26 || num < -26) num = num % 26;
+    let newLetter = letter + num;
+    if (letter >= 65 && letter <= 90) {
+      if (newLetter < 65) newLetter = 91 - (65 - newLetter);
+      if (newLetter > 90) newLetter = 64 + (newLetter - 90);
+      newUnicodeArray.push(newLetter);
+    } else if (letter >= 97 && letter <= 122) {
+      if (newLetter < 97) newLetter = 123 - (97 - newLetter);
+      if (newLetter > 122) newLetter = 96 + (newLetter - 122);
+      newUnicodeArray.push(newLetter);
+    } else {
+      newUnicodeArray.push(letter);
+    }
+  }
+  newStr = newUnicodeArray.map((x) => String.fromCharCode(x)).join("");
+  return newStr;
 };
 
 // Do not edit below this line
